b0637518134f1b4d4daed87edc89b48696813d68
Durvesh Vedak <durveshvedak@Durveshs-MBP.fios-router.home>
2019-04-25 21:14:41 -0400

    * spring boot intializer

---
+++ src/main/java/com/operr/springboot/operrlocations/OperrLocationsApplication.java
@@ -0,0 +1,13 @@
+package com.operr.springboot.operrlocations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperrLocationsApplication {git add
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperrLocationsApplication.class, args);
+	}
+
+}
